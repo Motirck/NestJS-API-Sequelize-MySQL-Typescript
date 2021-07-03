de07793abae1d761416a7db6679bc774daccde2e
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-07-03 15:22:41 -0300

    Created model Products and updated ProductController

--- api-products/src/products.controller.ts
+++ api-products/src/products.controller.ts
@@ -1,32 +1,38 @@
-import { Get, Param, Post, Body, Put, Delete } from "@nestjs/common";
-import { Controller } from "@nestjs/common";
+import { Get, Post, Put, Delete, Param, Controller, Body } from "@nestjs/common";
+import { Product } from "./product.model";
 
 @Controller('products')
 export class ProductsController {
 
+    products: Product[] = [
+        new Product('LIV01', 'Typescript Fundations', 29.95),
+        new Product('LIV02', 'Javascript Fundations', 39.05),
+        new Product('LIV03', 'Flutter Fundations', 59.14)
+    ];
+
     @Get()
-    getAll(): string {
-        return 'List all products';
+    getAll(): Product[] {
+        return this.products;
     }
 
     @Get(':id')
-    getOne(@Param() params): string {
-        return `Return data of product ${params.id}`;
+    getOne(@Param() params): Product {
+        return this.products[0];
     }
 
     @Post()
-    create(@Body() product): string {
-        console.log(product);
-        return 'Created product';
+    create(@Body() product: Product) {
+        product.id = 100;
+        this.products.push(product);
     }
 
     @Put()
-    update(@Body() product): string {
-        return 'Updated product';
+    update(@Body() product:Product) {
+        return product;
     }
 
     @Delete(':id')
-    delete(@Param() params): string {
-        return `Product ${params.id} deleted`;
+    delete(@Param() params) {
+        this.products.pop();
     }
 }
